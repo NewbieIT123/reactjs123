bc5a2f16254efd794564c75e3b41e66ad60b5a35
vu ky <hoangvuky12345@gmail.com>
2023-08-18 15:42:37 +0700

    new

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         What sup
+         What
         </p>
-      
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          test react
+        </a>
       </header>
     </div>
   );
